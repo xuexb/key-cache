37f135378dc17d4fe1426a2b4af393c51660758d
xuexb <fe.xiaowu@gmail.com>
2016-03-11 11:21:59 +0800

    update: 修改发布脚本

--- release.sh
+++ release.sh
@@ -17,7 +17,7 @@ then
 
     # git add -A
     # git commit -m "[build] $VERSION"
-    npm version $VERSION --message "[release] $VERSION"
+    npm version $VERSION --message "release: $VERSION"
 
     git push origin refs/tags/v$VERSION
     git push
